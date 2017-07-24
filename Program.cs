e89e10e65a4a78464924d20e53b23a5a5604e2a3
NameInikita <name.inikita@gmail.com>
2017-07-24 23:56:23 +0300

    Dynamic

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace var_dynamic
 {
@@ -11,10 +7,55 @@ namespace var_dynamic
         class MyClass
         {
             dynamic field;
+
+            public MyClass(dynamic arg)
+            {
+                field = arg;
+            }
+
+            public dynamic Field
+            {
+                get { return field; }
+                set { field = value; }
+            }
+
+            public dynamic Method(dynamic arg)
+            {
+                return arg;
+            }
+            dynamic[] array = new dynamic[3];
+
+            //индексатор
+
+            public dynamic this[dynamic index]
+            {
+                get { return array[index]; }
+                set { array[index] = value; }
+            }
         }
         static void Main(string[] args)
         {
+            MyClass my = new MyClass("First");
+            Console.WriteLine(my.Field);
+            my = new MyClass(new object().GetType());
+            Console.WriteLine(my.Field);
 
+            Console.WriteLine("char: {0}", my.Method('f'));
+            Console.WriteLine("string: {0}", my.Method("Second"));
+
+            my[0] = "one";
+            my[1] = 1;
+            my[2] = new object();
+
+            Console.WriteLine("\n");
+
+            for(int i=0;i<3;i++)
+            {
+                Console.Write(my[i]+"  ");
+                    
+            }
+
+            Console.ReadKey();
         }
     }
 }
